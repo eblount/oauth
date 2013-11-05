b7724106f6fce24fa4e61edcb2b11d237bf8aed0
Eric Blount <eblount@emmisdigital.com>
2013-11-05 12:07:09 -0600

    Migrate to Kohana 3.3/PSR-0

---
+++ classes/OAuth.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Oauth extends Kohana_OAuth {  }
--- views/oauth/twitter/tweet.php
+++ views/oauth/twitter/tweet.php
@@ -1,13 +1,13 @@
-<?php echo Form::open() ?>
+<?php echo FORM::open() ?>
 
 <dl>
-	<dt><?php echo Form::label('tweet', 'Update Twitter Status?') ?></dt>
-	<dd><?php echo Form::textarea('tweet', $tweet) ?></dd>
+	<dt><?php echo FORM::label('tweet', 'Update Twitter Status?') ?></dt>
+	<dd><?php echo FORM::textarea('tweet', $tweet) ?></dd>
 </dl>
 
-<?php echo Form::submit(NULL, 'Send') ?>
+<?php echo FORM::submit(NULL, 'Send') ?>
 
-<?php echo Form::close() ?>
+<?php echo FORM::close() ?>
 
 <?php if ($response): ?>
 <p>Response from Twitter:</p>
